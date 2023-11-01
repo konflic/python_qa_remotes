7d7d2209895f791783a7bee782d26f9fd9c117c3
konflic <konflic@mail.ru>
2023-11-01 22:38:52 +0300

    Версия 6.0

--- conftest.py
+++ conftest.py
@@ -2,15 +2,14 @@ import pytest
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import ChromiumOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.edge.options import Options as EdgeOptions
+from selenium.webdriver.ie.options import Options as IEOptions
 
 
 def pytest_addoption(parser):
-    parser.addoption(
-        "--browser",
-        default="chrome",
-        # choices=["chrome", "firefox", "MicrosoftEdge"]
-    )
-    parser.addoption("--executor", default="127.0.0.1")
+    parser.addoption("--browser", default="chrome")
+    parser.addoption("--executor", default="192.168.0.113")
 
 
 @pytest.fixture
@@ -22,6 +21,17 @@ def remote(request):
         options = ChromiumOptions()
         options.add_argument("headless=new")
         options.set_capability("browserName", browser)
+    elif browser == "firefox":
+        options = FirefoxOptions()
+    elif browser == "edge":
+        options = EdgeOptions()
+    elif browser == "ie":
+        options = IEOptions()
+    elif browser == "yandex":
+        options = ChromiumOptions()
+        options.set_capability("browserVersion", "100")
+    else:
+        raise ValueError
 
     driver = webdriver.Remote(
         command_executor=f"http://{executor}:4444/wd/hub",
